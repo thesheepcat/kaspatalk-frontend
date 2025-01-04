74658ae35175120dd91163e7604c8dc643a27b9a
thesheepcat <68336151+thesheepcat@users.noreply.github.com>
2025-01-04 10:37:20 +0100

    Revert "Added scrolltobottom when there is overflow"
    This reverts commit 3259c1677b59f47269e43a7360af9a5240671e58.

--- src/components/MainWindow/ChatContainer.jsx
+++ src/components/MainWindow/ChatContainer.jsx
@@ -4,7 +4,7 @@ import {useRef, useEffect, useContext, useState} from "react";
 import { GeneralContextProvider} from "../ContextProviders/GeneralContextProvider.jsx";
 
 const ChatContainer = () => {
-    const messageListRef = useRef(null);
+    const messageListRef = useRef(null);   
     const {selectedPeer, userAddress} = useContext(GeneralContextProvider);
     const [ messages, setMessages ] = useState([]);
 
@@ -14,41 +14,31 @@ const ChatContainer = () => {
             const response = await fetch(messagesEndpoint);
             if (!response.ok) {
                 throw new Error('Failed to fetch messages');
-        }
+        } 
             const fetchedMessages = await response.json();
             setMessages(fetchedMessages);
         } catch (responseError) {
             console.log(responseError)
         }
       };
+    
     // Fetch messages every 10 seconds
     useEffect(() => {
         if (selectedPeer != ""){
             fetchMessages();
-
         }
         const intervalId = setInterval(fetchMessages, 3000);
         // Cleanup function to clear the interval on component unmount
         return () => {
           clearInterval(intervalId);
         };
-
+        
       }, [selectedPeer]);
-
-    //every time message changes it makes sure to scroll to the bottom
-    useEffect(() => {
-        document.getElementById("chatBox").scrollIntoView({behavior: "smooth", block: "end"});
-
-
-    }, [messages]);
 
     return(
         <div className={styles.chatContainer}>
-            <div id={"chatBox"}
-                 className={styles.messageList}
-                 ref={messageListRef}
-            >
-                {messages.map((messageObj, i) => (<Message isReceivedMessage={(messageObj.receiver === userAddress) ? true : false} encryptedPayload={messageObj.message} timestamp={messageObj.block_time} key={i}/> ))}
+            <div className={styles.messageList} ref={messageListRef}>
+                {messages.map((messageObj, i) => (<Message isReceivedMessage={(messageObj.receiver == userAddress) ? true : false} encryptedPayload={messageObj.message} timestamp={messageObj.block_time} key={i}/> ))}
             </div>
         </div>
     )
