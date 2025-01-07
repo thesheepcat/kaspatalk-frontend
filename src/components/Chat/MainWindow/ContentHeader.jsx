6a183729e31e68a6d54208bf7375c960d8c7def7
ffrn <you@example.com>
2025-01-07 02:43:55 +0100

    migrated ContentHeader to MUI

--- src/components/Chat/MainWindow/ContentHeader.jsx
+++ src/components/Chat/MainWindow/ContentHeader.jsx
@@ -1,9 +1,21 @@
-import styles from "./ContentHeader.module.css";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisV, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useContext, useEffect, useState } from "react";
 import { GeneralContextProvider } from "../../ContextProviders/GeneralContextProvider.jsx";
 import { toSvg } from "jdenticon";
+import Box from "@mui/material/Box";
+import {Image} from "@mui/icons-material";
+import {ImageListItem, Typography} from "@mui/material";
+import {
+    ContentHeaderContainerBoxStyle,
+    DetailsContainerBoxStyle,
+    DetailsSpanContainerTypographyStyle,
+    DetailsTitleContainerTypographyStyle,
+    IconsContainerBoxStyle,
+    ImageContainerBoxStyle,
+    ImageContainerImageStyle
+} from "./ContentHeader.styles.js";
 
 const ContentHeader = () => {
     const { selectedPeer } = useContext(GeneralContextProvider);
@@ -19,19 +31,31 @@ const ContentHeader = () => {
     }, [selectedPeer])
 
     return(
-        <div className={styles.contentHeader}>
-                <div className={styles.image}>
-                    <img src={peerImage} alt="" />
-                </div>
-                <div className= {styles.details}>
-                    <span className={styles.detailsTitle}>{selectedPeer}</span>
-                    <span className={styles.detailsSpan}>last seen 10 minutes ago</span>
-                </div>
-                <div className={styles.icons}>
-                    <FontAwesomeIcon icon={faSearch} className={styles.toggleButtonIcon} />
-                    <FontAwesomeIcon icon={faEllipsisV} className={styles.toggleButtonIcon} />      
-                </div>
-        </div>
+        <Box className={"styles.contentHeader"}
+             sx={ContentHeaderContainerBoxStyle}>
+                <Box className={"image"}
+                    sx={ImageContainerBoxStyle}>
+                    <ImageListItem
+                        sx={ImageContainerImageStyle}>
+                        <img src={peerImage} alt="peerImage" />
+                    </ImageListItem>
+
+                </Box>
+                <Box className= {"styles.details"}
+                    sx={DetailsContainerBoxStyle}>
+                    <Typography component={"span"}
+                                sx={DetailsTitleContainerTypographyStyle}
+                                className={"styles.detailsTitle"}>{selectedPeer}</Typography>
+                    <Typography component={"span"}
+                                sx={DetailsSpanContainerTypographyStyle}
+                                className={"styles.detailsSpan"}>last seen 10 minutes ago</Typography>
+                </Box>
+                <Box className={"styles.icons"}
+                     sx={IconsContainerBoxStyle}>
+                    <FontAwesomeIcon icon={faSearch} className={"styles.toggleButtonIcon"} />
+                    <FontAwesomeIcon icon={faEllipsisV} className={"styles.toggleButtonIcon"} />
+                </Box>
+        </Box>
     )
 }
 
---
+++ src/components/Chat/MainWindow/ContentHeader.styles.js
@@ -0,0 +1,50 @@
+export const ContentHeaderContainerBoxStyle = {
+    position: 'relative',
+    backgroundColor: '#ffffff',
+    padding: '0 1rem',
+    display: 'flex',
+    alignItems: 'center',
+    height: '5rem',
+    zIndex: 1,
+}
+export const ImageContainerBoxStyle = {
+    width: '3rem',
+    height: '3rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+    maxWidth: '100%',
+    objectFit: 'cover',
+}
+export const ImageContainerImageStyle = {
+    borderRadius: '50%',
+    maxWidth: '100%',
+    objectFit: 'cover',
+}
+export const DetailsContainerBoxStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '1rem',
+    gap: '.1rem',
+}
+export const DetailsTitleContainerTypographyStyle = {
+    fontSize: '1.2rem',
+    fontWeight: 500,
+}
+export const DetailsSpanContainerTypographyStyle = {
+    fontSize: '1rem',
+    fontWeight: 500,
+    color: '#707579',
+    opacity: 0.7,
+}
+export const IconsContainerBoxStyle = {
+    marginLeft: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '3rem',
+    fontSize: '1.4rem',
+    color: '#707579',
+    opacity: 0.6,
+    cursor: 'pointer'
+}
